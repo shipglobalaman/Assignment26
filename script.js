65c842351461293a5e121f3d2a17b90048eecebe
Aman <aman.tripathi@shipglobal.in>
2024-09-26 18:31:50 +0530

    Worked on UI

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ dropArea.addEventListener("drop",function(e){
     inputFile.files=e.dataTransfer.files ;
     uploadImage();
 });
+
+// This code is for Sidebar
 const menuIcon = document.querySelector('.menu-icon');
 const sidebar = document.querySelector('.sidebar');
 const closeBtn = document.querySelector('.close-btn');
@@ -47,4 +49,31 @@ sidebar.addEventListener('mouseleave', () => {
 
 closeBtn.addEventListener('click', () => {
     sidebar.classList.remove('open');
+});
+
+// document.querySelectorAll('.upload-button').forEach(button => {
+//     button.addEventListener('click', function() {
+//         this.nextElementSibling.click();
+//     });
+// });
+const dropArea1 = document.getElementById("drop-area");
+const inputFile1 = document.getElementById("input-file");
+const imageView1 = document.getElementById("img-view");
+
+inputFile1.addEventListener("change", uploadImage1);
+
+function uploadImage() {
+    let imgLink1 = URL.createObjectURL(inputFile.files[0]);
+    imageView.style.backgroundImage = `url("${imgLink1}")`; 
+    imageView.textContent="";
+    imageView.style.border=0;
+  
+}
+dropArea.addEventListener("dragover",function(f){
+ f.preventDefault();
+});
+dropArea.addEventListener("drop",function(f){
+    f.preventDefault();
+    inputFile.files=e.dataTransfer.files ;
+    uploadImage();
 });
